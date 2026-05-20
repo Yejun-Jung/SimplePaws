350bd8177dd33523efccc739f81428d04d1e209f
unknown <once6474@naver.com>
2026-05-20 21:08:58 +0900

    client.js update

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const client = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8081',
-  withCredentials: true,
+    baseURL: import.meta.env.VITE_API_URL || 'http://115.68.226.78:8081',
+    withCredentials: true,
 })
 
 export default client
